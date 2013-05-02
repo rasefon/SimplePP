6daa9a102fcee0db0ed8ecad27dd4085f6d6ce1a
Rasefon <rasefon@gmail.com>
2013-05-02 10:54:21 +0800

    add context.h

---
+++ context.h
@@ -0,0 +1,30 @@
+enum transction_mode {
+  AUTO_C,
+  MAN,
+  AUTO_RB,
+  NONE
+};
+
+enum attribute_type {
+  T_M,
+  T_C,
+  NA
+};
+
+//struct Class_info {
+  //char* class_name;
+  //enum transction_mode tm;
+  //struct Test_method_info* tm_list_head;
+//};
+
+//struct Test_method_info {
+  //char* method_name;
+  //enum transction_mode tm;
+  //struct Class_info* parent_class;
+  //struct Test_method_info* next;
+//};
+
+//struct ID_value {
+  //char* id_str;
+  //struct ID_value* next;
+//};
